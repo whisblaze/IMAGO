6b0fa3d3a232f102c8d6c6d75c116451979d78f9
koliphan2 <koliphant@otincubator.com>
2026-08-18 12:00:55 -0500

    Translate Spanish Lorthemar Theron

--- locales/esES/data/npcs/midnight_npcs.lua
+++ locales/esES/data/npcs/midnight_npcs.lua
@@ -5,7 +5,21 @@
 if GetLocale() ~= "esES" then return end
 
 -- === CAT_QUELTHALAS ===
+IMAGOdb.npcs.CAT_QUELTHALAS["lor_themar_theron_es_midnight"].name = "Lor'themar Theron [es]"
+IMAGOdb.npcs.CAT_QUELTHALAS["lor_themar_theron_es_midnight"].aliases = {}
+IMAGOdb.npcs.CAT_QUELTHALAS["lor_themar_theron_es_midnight"].race = "Elfo de sangre"
+IMAGOdb.npcs.CAT_QUELTHALAS["lor_themar_theron_es_midnight"].lore = [[
+Siendo un Forestal de corazón, Lor'themar Theron no eligió liderar a los elfos de sangre. Cuando Quel'thalas cayó por la Plaga en la Tercera Guerra, fue nombrado como regente por el Príncipe Kael'thas, en parte debido a la falta de otros candidatos supervivientes. 
 
+Fue forzado a tomar decisiones difíciles, como aliar a los elfos de sangre con la Horda, exiliar a los altos elfos para combatir los disturbios y liderar la batalla contra Kael'thas luego de la traición del príncipe. El pragmatismo de Lor'themar y su astucia estratégica lo convirtieron, no obstante, en un líder inspirador, ganándole el respeto de amigos y enemigos por igual. 
+
+Un firme defensor de la paz, Lor'themar ahora sirve como regente de Quel'thalas y como miembro del Consejo de la Horda. Su lealtad permanece primero y por encima de todo en su gente. 
+
+Ahora, con Quel'thalas en peligro una vez más, no se detendrá ante nada para defender a su gente y su ciudad en contra de las fuerzas invasoras del Vacío.
+]]
+IMAGOdb.npcs.CAT_QUELTHALAS["lor_themar_theron_es_midnight"].zones = {"Ciudad de Lunargenta", "Bosque Canción Eterna", "Isla de Quel'Danas", "Harandar"}
+IMAGOdb.npcs.CAT_QUELTHALAS["lor_themar_theron_es_midnight"].source = "Karstan"
+IMAGOdb.npcs.CAT_QUELTHALAS["lor_themar_theron_es_midnight"].timeline = IMAGOdb.timelines.LORTHEMAR_THERON
 -- === CAT_LIGHT ===
 
 -- === CAT_AMANI ===
--- locales/esES/data/timelines.lua
+++ locales/esES/data/timelines.lua
@@ -1,2 +1,49 @@
 ﻿if GetLocale() ~= "esES" then return end
 
+IMAGOdb.timelines.LORTHEMAR_THERON = {
+    {
+        era = "WC2",
+        text = [[Defendió Lunargenta con éxito bajo el liderazgo de Sylvanas Brisaveloz y fue ascendido a Señor Forestal.]],
+    },
+    {
+        era = "WC3",
+        text = [[Luego de sobrevivir la destrucción de Quel'thalas, el Príncipe Kael'thas lo ascendió como señor regente. Recuperó Lunargenta de la Plaga con éxito.]],
+    },
+    {
+        era = "TBC",
+        text = [[Guio a los elfos de sangre para que se unieran a la Horda, tomó la decisión de exiliar a los altos elfos y destituyó a Kael'thas luego de su traición. Tras la guerra civil, Lor'themar se mantuvo como señor regente.]],
+    },
+    {
+        era = "WotLK",
+        text = [[Aceptó a regañadientes enviar tropas élficas a Rasganorte para ayudar a la Horda en la batalla contra el Rey Exánime y ayudó a recuperar la antigua espada Quel'delar.]],
+    },
+    {
+        era = "Pre-MoP",
+        text = [[Se rehusó a participar personalmente en el ataque a Theramore, provocando la ira de Garrosh Grito Infernal.]],
+    },
+    {
+        era = "MoP",
+        text = [[Negoció con el Rey Varian Wrynn para reincorporarse a la Alianza luego de discrepar con el liderazgo de Garrosh. Luego de la purga de Dalaran, se mantuvo en la Horda y jugó un rol clave en la rebelión en contra de Garrosh Grito Infernal.]],
+    },
+    {
+        era = "BfA",
+        text = [[Logró invitar con éxito a los Nocheterna de Suramar a unirse a la Horda. Personalmente, lideró las fuerzas de la Horda en la campaña contra la Reina Azshara y se unió a la rebelión en contra de Sylvanas Brisaveloz. Luego de la Cuarta Guerra se unió al recientemente formado Consejo de la Horda como uno de sus líderes.]],
+    },
+    {
+        era = "Pre-DF",
+        text = [[Se casó con Thalyssra e invitó a los líderes de la Horda y la Alianza a la boda en un acto de apoyo de la paz entre ambas facciones.]],
+    },
+    {
+        era = "DF",
+        text = [[Se unió en la defensa de Amirdrassil.]],
+    },
+    {
+        era = "Pre-TWW",
+        text = [[Viajó por las islas de Azeroth con Thalyssra durante su tardía luna de miel. Escuchó la Canción Radiante mientras Wrathion le enseñaba las Islas Dragón.]],
+    },
+    {
+        era = "Midnight",
+        text = [[Lideró a los elfos de sangre en la defensa de Lunargenta en contra del Vacío.]],
+    }
+}
+
